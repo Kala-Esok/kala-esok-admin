f4f195ff91f9c333a51c3a0314a07668e3fb8790
Akmal Bintang Budiawan <akmalbintang33@gmail.com>
2026-04-26 17:13:57 +0700

    :sparkles: feat: first mockup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  ...authOptions,
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+console.log('NextAuth: Secret detected:', !!process.env.NEXTAUTH_SECRET);
 
 export { handler as GET, handler as POST };
